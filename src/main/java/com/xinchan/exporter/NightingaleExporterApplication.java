64533c9cacb0b750c3b4839939bab9839159d853
hibernate-pano <362472407@qq.com>
2022-07-25 21:16:28 +0800

    Initial commit

---
+++ src/main/java/com/xinchan/exporter/NightingaleExporterApplication.java
@@ -0,0 +1,13 @@
+package com.xinchan.exporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NightingaleExporterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NightingaleExporterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xinchan/exporter/NightingaleExporterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xinchan.exporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NightingaleExporterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
